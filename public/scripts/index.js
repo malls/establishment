744a8c687beae3a3be314560dfe99e5f59b3f9e3
Forrest Almasi <forrest.almasi@gmail.com>
2019-12-01 00:15:54 -0500

    start contact page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,45 +1,92 @@
-(function(Papa) {
-
-    let dummyData = [{
-        caption: 'cool',
-        photographer: 'forrest',
-        url: 'index2.html',
-        image: 'exxie merckx.jpg'
-    }]
+(function(window, document, Papa) {
     
-    function makeIndexBody(content) {
-        const templateWrapper = `
-        <div class="parent-horizontal-flex">
-            ${content}
-        </div>
-        `;
-        return templateWrapper;
+    let page = 'index';
+
+    const path = window.location.pathname;
+    const specificPage = window.location.search ? `-${window.location.search.split('=')[1]}` : ''
+
+    if (path.includes('archive')) {
+        page = 'archive';
+    } else if (path.includes('project-highlight')) {
+        page = 'project-highlight';
+    } else if (path.includes('runway-highlight')) {
+        page = 'runway-highlight';
+    } else if (path.includes('runway')) {
+        page = 'runway';
+    } else if (path.includes('contact')) {
+        page = 'contact';
     }
 
-    function createIndexPhotos(data) {
-        let body = '';
+    const templateMethods = {
+        index(data) {
+            let body = '';
 
-        data.forEach(item => {
-            body += `
-                <a href="${item.url}">
-                <img class="full-height-image" src="images/${item.image}">
-                <div class="sideways credit-text pad-credit-text">
-                    <span class="pad-name">${item.caption}</span>
-                    <span class="pad-name">photography ${item.photographer}</span>
-                </div>
-            </a>
-        `
-        });
-        return body;
+            data.forEach(item => {
+                body += `
+                    <a href="${item.url}">
+                    <img class="full-height-image" src="images/${item.image}">
+                    <div class="sideways credit-text pad-credit-text">
+                        <span class="pad-name">${item.caption}</span>
+                        <span class="pad-name">photography ${item.photographer}</span>
+                    </div>
+                </a>
+            `
+            });
+
+            return `<div class="parent-horizontal-flex">
+                ${body}
+            </div>
+            `;
+        },
+        contact(data) {
+            //name,title,email,instagram,imageurl
+            let body = '';
+
+            data.forEach(item => {
+                `<div class="contact-container">
+                    <img src="${data.imageurl}" alt="">
+
+                    <div class="contact-info">
+                        ${data.name}
+                        <br>
+                        <br>
+                        ${data.title}
+                        <br>
+                        <br>
+                        Lorem Ipsum Dorem
+                        <br>
+                        Morit
+                        <br>
+                        Favim Isig iopsi
+                        <br>
+                        <a href="https://instagram.com/${data.instagram}">${data.instagram}</a>
+                        <br>
+                        <br>
+                        <a href="mailto:${data.email}">${data.email}</a>
+
+                    </div>`
+
+            });
+            
+
+
+            return `<div class="contact-section">
+                ${body}
+            </div>
+            `;
+
+        }
     }
 
-    Papa.parse('http://localhost:3000/csvs/index.csv', {
+
+
+
+    Papa.parse(`http://localhost:3000/csvs/${page}${specificPage}.csv`, {
         download: true,
         header: true,
         complete: function(results) {
-            document.getElementById('replacer').innerHTML = makeIndexBody(createIndexPhotos(results.data));
+            document.getElementById('replacer').innerHTML = templateMethods[page.replace('-','')](results.data);
         }
     })
 
-
-})(Papa);
+})(window, document, Papa);
